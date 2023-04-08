c0b41f394299a13aeb9eb91f019ec3e756364421
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-08 18:16:51 +0530

    files updated

--- fithelp.2/src/components/Hero/Hero.jsx
+++ fithelp.2/src/components/Hero/Hero.jsx
@@ -1,34 +1,29 @@
-import React from 'react'
-import './Hero.css'
-import Header from '../Header/Header'
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
 
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="left-h">
+        <Header />
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
+        {/* hero-head */}
+        <div className="hero-text">
+          <div>
+            <span>Shape </span>
+            <span>Your</span>
+          </div>
+        </div>
+      </div>
 
-    <div className="left-h">
-    
-    
-    <Header/>
-    <div className="the-best-ad">
-        <div></div>
-        <span>
-            the best fitness club in the town
-        </span>
+      <div className="right-h"> right side</div>
     </div>
+  );
+};
 
-
-    
-    
-    </div>
-    
-    
-    <div className="right-h"> right side</div>
-
-
-    </div>
-
-  )
-}
-
-export default Hero
+export default Hero;
