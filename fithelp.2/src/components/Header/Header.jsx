9bf4fd641351d84941f6ea28bc2410a7265b9a8b
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-04-08 15:13:09 +0530

    files updated

--- fithelp.2/src/components/Header/Header.jsx
+++ fithelp.2/src/components/Header/Header.jsx
@@ -8,6 +8,14 @@ const Header = () => {
    
    
    <img src={Logo} alt=""  className='logo'/>
+
+   <ul className='header-menu'>
+    <li>Home</li>
+    <li>Programs</li>
+    <li>Why us</li>
+    <li>Plans</li>
+    <li>Testimonials</li>
+   </ul>
    
    </div>
 
--- fithelp.2/src/components/Hero/Hero.jsx
+++ fithelp.2/src/components/Hero/Hero.jsx
@@ -10,6 +10,8 @@ const Hero = () => {
     
     
     <Header/>
+
+    
     
     
     </div>
